44a2235d272c39aa599540894fa5a5554afb437e
Leor Goldberg <leorgoldberg7@gmail.com>
2024-05-15 12:53:40 -0400

    Added reportgenerator, wrapped in a bash script. Can launch from test directory, and script also automatically runs unit tests. 38% line coverage right now.

--- PingBoard.Tests/obj/Debug/net8.0/PingBoard.Tests.AssemblyInfo.cs
+++ PingBoard.Tests/obj/Debug/net8.0/PingBoard.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PingBoard.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6345ffeb84da49ecc6d01ff02c8b59a89bc00dfa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41dae0aecf18cfa49e3ddc78832a312963e0d8f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PingBoard.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PingBoard.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
