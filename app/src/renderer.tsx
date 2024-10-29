913100c5b4f6b47b00811db8c2c9b0e789083db0
Leor Goldberg <leorgoldberg7@gmail.com>
2024-10-29 00:10:51 -0400

    Project structure shifted to accomodate electron, dev hotloading done through electron and not vite now. Launches as app, styling/formatting needs adjustment

--- app/src/renderer.tsx
+++ app/src/renderer.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from "./dashboard/App"
 import './index.css'
+
+console.log('👋 This message is being logged by "renderer.ts", included via Vite');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
