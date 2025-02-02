db49c21f45ab5656760efb9c325f57c5661e66ed
Leor Goldberg <leorgoldberg7@gmail.com>
2025-02-02 01:30:47 -0500

    Probe form uses shadcn dialog box now. Need to deal with form state/styling.

--- app/src/dashboard/pages/ProbeMenu/CommonInput/ProbeFormCard.tsx
+++ app/src/dashboard/pages/ProbeMenu/CommonInput/ProbeFormCard.tsx
@@ -1,11 +1,19 @@
 import {useState} from 'react';
 import {Card} from '@/components/ui/card';
 import {Button} from '@/components/ui/button'
-import { SquarePlay } from 'lucide-react';
+import { SquarePlay, SquarePlus } from 'lucide-react';
 import { ProbeSelectionDropDown } from './ProbeSelectionDropDown';
 import { probes, Probe } from './ProbeFormTypes';
 import { ProbeFormTypeDecider} from './ProbeFormTypeDecider';
-import { Dialog } from '@/components/ui/dialog';
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from '@/components/ui/dialog';
 
 
 export function ProbeFormCard(){
@@ -14,23 +22,28 @@ export function ProbeFormCard(){
 
     return (
         <Dialog>
-            <div>
-                <ProbeSelectionDropDown
+        <DialogTrigger asChild>
+        <Button variant="outline">Add Probe<SquarePlus/></Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[525px]">
+        <DialogHeader>
+            <DialogTitle>Choose and configure Probe</DialogTitle>
+            <DialogDescription>
+            Select an operation and then configure the probe. Click "Send Probe" when done.
+            </DialogDescription>
+        </DialogHeader>
+        <ProbeSelectionDropDown
                     probes={probes}
                     onSelection={setProbeType}
                     probeSelection={probeType    
                 }/>
-            </div>
-            <div>
-                <Button>
-                    Send Probe 
-                    <SquarePlay/>
-                </Button>
-            </div>
-            <div>  
-                <ProbeFormTypeDecider probe={probeType} /> 
-            </div>
+            <ProbeFormTypeDecider probe={probeType} /> 
+        <DialogFooter>
+            <Button type="submit">Send Probe<SquarePlay/></Button>
+        </DialogFooter>
+        </DialogContent>
         </Dialog>
+        
     )
 }
 
--- app/src/dashboard/pages/ProbeMenu/ProbeManagementPage.tsx
+++ app/src/dashboard/pages/ProbeMenu/ProbeManagementPage.tsx
@@ -2,6 +2,7 @@ import { ProbeManagementTable } from './ProbesTable/ProbeManagementTable';
 import {useState, useEffect } from 'react';
 import { ProbeStatus, columns } from './ProbesTable/ProbeManagementTableColumns';
 import { AddProbeButton } from './CommonInput/AddProbeButton';
+import { ProbeFormCard } from './CommonInput/ProbeFormCard';
 
 export default function ProbeManagementPage(){
     const [probeStatuses, setProbeStatuses] = useState<ProbeStatus[]>([{
@@ -14,7 +15,7 @@ export default function ProbeManagementPage(){
     return (
         <div className='probe-management-page flex justify-center items-center'>
             <div className= "btn add-probe">
-                <AddProbeButton />
+                <ProbeFormCard />
             </div>
             <div className="tbl probe-management">
                 <ProbeManagementTable columns={columns} data={probeStatuses}/>
